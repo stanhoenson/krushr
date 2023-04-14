f13ea426c9e4d856643498c1a0d70f388649db95
Stan Hoenson <s.hoenson@protonmail.com>
2023-04-14 22:41:01 +0200

    Change models

--- backend/krushr/internal/models/index.go
+++ backend/krushr/internal/models/index.go
@@ -1,69 +1,63 @@
 package models
 
-// TODO add validation tags https://github.com/go-playground/validator
+type Route struct {
+	ID               uint              `json:"id"`
+	Name             string            `json:"name"`
+	Images           []Image           `json:"images"`
+	Details          []Detail          `json:"details"`
+	Links            []Link            `json:"links"`
+	Categories       []Category        `json:"categories"`
+	Status           Status            `json:"status"`
+	PointsOfInterest []PointOfInterest `json:"points_of_interest"`
+	User             User              `json:"user"`
+}
+
+type Image struct {
+	ID   uint   `json:"id"`
+	Path string `json:"path"`
+}
+
+type Detail struct {
+	ID   uint   `json:"id"`
+	Text string `json:"text"`
+}
+
+type Link struct {
+	ID  uint   `json:"id"`
+	URL string `json:"url"`
+}
+
+type Category struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Position uint   `json:"position"`
+}
+
+type Status struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
+
+type PointOfInterest struct {
+	ID         uint       `json:"id"`
+	Name       string     `json:"name"`
+	Longitude  float64    `json:"longitude"`
+	Latitude   float64    `json:"latitude"`
+	Images     []Image    `json:"images"`
+	Details    []Detail   `json:"details"`
+	Links      []Link     `json:"links"`
+	Categories []Category `json:"categories"`
+	User       User       `json:"user"`
+}
+
 type User struct {
-	ID       uint   `gorm:"primaryKey" json:"id"`
-	Email    string `gorm:"not null" json:"email"`
-	Password string `gorm:"not null" json:"password"`
-	RoleID   uint   `gorm:"not null" json:"role_id"`
+	ID       uint   `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 	Role     Role   `json:"role"`
 }
 
 type Role struct {
-	ID   uint   `gorm:"primaryKey" json:"id"`
-	Role string `gorm:"not null" json:"role"`
-}
-
-type Route struct {
-	ID               uint               `gorm:"primaryKey" json:"id"`
-	Title            string             `gorm:"not null" json:"title"`
-	StatusID         uint               `gorm:"not null" json:"status_id"`
-	Status           Status             `json:"status"`
-	UserID           uint               `gorm:"not null" json:"user_id"`
-	User             User               `json:"user"`
-	PointsOfInterest []*PointOfInterest `gorm:"many2many:routes_points_of_interest" json:"points_of_interest"`
-	Entries          []*Entry           `gorm:"many2many:entries_routes" json:"entries"`
-	Categories       []*Category        `gorm:"many2many:categories_routes" json:"categories"`
-}
-
-type Status struct {
-	ID     uint   `gorm:"primaryKey" json:"id"`
-	Status string `gorm:"not null" json:"status"`
-}
-
-type PointOfInterest struct {
-	ID         uint        `gorm:"primaryKey" json:"id"`
-	Title      string      `gorm:"not null" json:"title"`
-	Longitude  float64     `gorm:"type:decimal(9,6)" json:"longitude"`
-	Latitude   float64     `gorm:"type:decimal(9,6)" json:"latitude" binding:"required,latitude"`
-	Categories []*Category `gorm:"many2many:categories_points_of_interest" json:"categories"`
-	Routes     []*Route    `gorm:"many2many:routes_points_of_interest" json:"routes"`
-	Entries    []*Entry    `gorm:"many2many:entries_points_of_interest" json:"entries"`
-}
-
-type Type struct {
-	ID   uint   `gorm:"primaryKey" json:"id"`
-	Type string `gorm:"not null" json:"type"`
-	Icon string `json:"icon"`
-}
-
-type Entry struct {
-	ID               uint               `gorm:"primaryKey" json:"id"`
-	Content          string             `gorm:"not null" json:"content"`
-	Hyperlink        string             `gorm:"not null" json:"hyperlink"`
-	TypeID           uint               `gorm:"not null" json:"type_id"`
-	Type             Type               `json:"type"`
-	Routes           []*Route           `gorm:"many2many:entries_routes" json:"routes"`
-	PointsOfInterest []*PointOfInterest `gorm:"many2many:entries_points_of_interest" json:"points_of_interest"`
-}
-
-type Category struct {
-	ID               uint               `gorm:"primaryKey" json:"id"`
-	Category         string             `gorm:"not null" json:"category"`
-	Icon             string             `gorm:"not null" json:"icon"`
-	Weight           int                `gorm:"not null" json:"weight"`
-	TypeID           uint               `json:"type_id"`
-	Type             Type               `json:"type"`
-	PointsOfInterest []*PointOfInterest `gorm:"many2many:categories_points_of_interest" json:"points_of_interest"`
-	Routes           []*Route           `gorm:"many2many:categories_routes" json:"routes"`
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
 }
