f5a094d9a92ae975f7e8f0d47cc0aab822b36a71
Stan Hoenson <s.hoenson@protonmail.com>
2023-04-16 20:11:27 +0200

    Add some more body structs

--- backend/krushr/internal/handlers/routes.go
+++ backend/krushr/internal/handlers/routes.go
@@ -14,7 +14,7 @@ import (
 )
 
 func PutRoute(c *gin.Context) {
-	var updatedRoute models.Route
+	var updatedRoute models.PutRouteBody
 
 	if err := c.BindJSON(&updatedRoute); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
--- backend/krushr/internal/models/bodies.go
+++ backend/krushr/internal/models/bodies.go
@@ -4,3 +4,29 @@ type PostRouteBody struct {
 	Name     string `json:"name" binding:"required"`
 	StatusID uint   `json:"status_id" binding:"required"`
 }
+
+type PutRouteBody struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name" binding:"required"`
+	StatusID uint   `json:"status_id" binding:"required"`
+}
+
+type PostImageBody struct {
+	Path string `json:"path" binding:"required"`
+}
+
+type PostDetailBody struct {
+	Text string `json:"text" binding:"required"`
+}
+
+type PostLinkBody struct {
+	URL string `json:"url" binding:"required"`
+}
+
+type PostCategoryBody struct {
+	Name     string `json:"name" binding:"required"`
+	Position uint   `json:"position" binding:"required"`
+}
+type PostStatusBody struct {
+	Name string `json:"name" binding:"required"`
+}
