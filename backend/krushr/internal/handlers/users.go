811146afa692dc0ff1f0d890d790348a671fe1fe
Stan Hoenson <s.hoenson@protonmail.com>
2023-05-05 22:20:37 +0200

    Add users me endpoint

--- backend/krushr/internal/handlers/users.go
+++ backend/krushr/internal/handlers/users.go
@@ -1,17 +1,37 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/stanhoenson/krushr/internal/constants"
 	"github.com/stanhoenson/krushr/internal/database"
 	"github.com/stanhoenson/krushr/internal/models"
 	"github.com/stanhoenson/krushr/internal/services"
+	"github.com/stanhoenson/krushr/internal/utils"
 	"github.com/stanhoenson/krushr/internal/wrappers"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm/clause"
 )
+
+func GetMe(c *gin.Context) {
+
+	user, err := utils.GetUserFromContext(c)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Error retrieving " + utils.GetTypeString(&models.User{})})
+	}
+	entity, err := services.GetEntityByIDWithAssociations[models.User](user.ID, clause.Associations)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Error retrieving " + utils.GetTypeString(entity)})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, entity)
+}
 
 func RegisterUserRoutes(r *gin.Engine) {
 	routes := r.Group("/users")
 	{
+		routes.GET("/me", GetMe)
 		routes.GET("", wrappers.RoleWrapper([]string{constants.AdminRoleName}, GetAllDefault[models.User]))
 		routes.POST("", wrappers.RoleWrapper([]string{constants.AdminRoleName}, func(ctx *gin.Context) {
 			Post(ctx, func(c *gin.Context, requestBody *models.PostUserBody) error {
