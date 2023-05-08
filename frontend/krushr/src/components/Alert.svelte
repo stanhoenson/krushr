7555f41a14a8c70f199ad753ef8e93717537c3ee
Stan Hoenson <s.hoenson@protonmail.com>
2023-05-08 21:02:49 +0200

    WIP

--- frontend/krushr/src/components/Alert.svelte
+++ frontend/krushr/src/components/Alert.svelte
@@ -3,11 +3,12 @@
 
   export let onClose: null | (() => any) = null;
   export let type: "error" | "success" = "error";
+  console.log(onClose !==null)
 </script>
 
 <div id="half-width" class={`alert block ${type} soft`}>
   <span class="message"><slot /></span>
-  {#if onClose}
+  {#if onClose !== null}
     <button class="icon" on:click={onClose}><XMark /></button>
   {/if}
 </div>
